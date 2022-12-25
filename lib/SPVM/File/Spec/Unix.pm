5b51ba29a3d64d2918106cab962689db90860f6d
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-12-26 08:51:41 +0900

    Added File::Spec::Unix, File::Spec::Win32

---
+++ lib/SPVM/File/Spec/Unix.pm
@@ -0,0 +1,45 @@
+package SPVM::File::Spec::Unix;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::File::Spec::Unix - foo
+
+=head1 Description
+
+C<SPVM::File::Spec::Unix> is the L<SPVM>'s C<File::Spec::Unix> class.
+
+=head1 Usage
+
+  use File::Spec::Unix;
+  
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 Yuki Kimoto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
---
+++ lib/SPVM/File/Spec/Win32.pm
@@ -0,0 +1,45 @@
+package SPVM::File::Spec::Win32;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::File::Spec::Win32 - foo
+
+=head1 Description
+
+C<SPVM::File::Spec::Win32> is the L<SPVM>'s C<File::Spec::Win32> class.
+
+=head1 Usage
+
+  use File::Spec::Win32;
+  
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 Yuki Kimoto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
