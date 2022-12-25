0032ce8e94783888de759041d2473c7fbd6d6995
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-12-26 08:38:57 +0900

    Initial commit

---
+++ lib/SPVM/File/Spec.pm
@@ -0,0 +1,45 @@
+package SPVM::File::Spec;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 Name
+
+SPVM::File::Spec - foo
+
+=head1 Description
+
+C<SPVM::File::Spec> is the L<SPVM>'s C<File::Spec> class.
+
+=head1 Usage
+
+  use File::Spec;
+  
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 Yuki Kimoto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
