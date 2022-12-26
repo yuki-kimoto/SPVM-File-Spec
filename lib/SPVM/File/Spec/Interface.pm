7dc7645058c04054e9143d7beb4dce8269e0d7f4
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-12-26 10:44:08 +0900

    Added File::Spec::Interface

---
+++ lib/SPVM/File/Spec/Interface.pm
@@ -0,0 +1,45 @@
+package SPVM::File::Spec::Interface;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::File::Spec::Interface - foo
+
+=head1 Description
+
+C<SPVM::File::Spec::Interface> is the L<SPVM>'s C<File::Spec::Interface> class.
+
+=head1 Usage
+
+  use File::Spec::Interface;
+  
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright 2022-2022 Yuki Kimoto, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
