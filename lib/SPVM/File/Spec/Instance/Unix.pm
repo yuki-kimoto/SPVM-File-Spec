cd3ddef6c2efe80e8060362d01420ecac7d78836
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-07 08:29:56 +0900

    Add docs for File::Spec::Instance::UNIX.

--- lib/SPVM/File/Spec/Instance/Unix.pm
+++ lib/SPVM/File/Spec/Instance/Unix.pm
@@ -33,37 +33,61 @@ C<static method new : File::Spec::Instance::Unix ();>>
 
 C<method devnull : string ();>
 
+Returns a string representation of the null device. "/dev/null" on UNIX.
+
 =head2 rootdir
 
 C<method rootdir : string ();>
+
+Returns a string representation of the root directory. "/" on UNIX.
 
 =head2 curdir
 
 C<method curdir : string ();>
 
+Returns a string representation of the current directory. "." on UNIX.
+
 =head2 updir
 
 C<method updir : string ();>
+
+Returns a string representation of the parent directory. ".." on UNIX.
 
 =head2 canonpath
 
 C<method canonpath : string ($path : string);>
 
+Returns a logical cleanup of a path from the path $path.
+
+See L<File::Spec::Unix/"canonpath"> for details.
+
 =head2 catdir
 
 C<method catdir : string ($parts : string[]);>
+
+Concatenates two or more directory names $parts to form a complete path ending with a directory, and returns it.
+
+See L<File::Spec::Unix/"catdir"> for details.
 
 =head2 catfile
 
 C<method catfile : string ($parts : string[]);>
 
+Concatenate one or more directory names and a filename $parts to form a complete path ending with a filename, and returns it.
+
 =head2 no_upwards
 
 C<method no_upwards : string[] ($parts : string[]);>
 
+Given a list of file names $parts, strip out those that refer to a parent directory, and returns it. (Does not strip symlinks, only '.', '..', and equivalents.)
+
 =head2 file_name_is_absolute
 
 C<method file_name_is_absolute : int ($path : string);>
+
+Takes as argument a path $path and returns 1 if it is an absolute path. Otherwise returns 0.
+
+See L<File::Spec::Unix/"file_name_is_absolute"> for details.
 
 =head2 file_name_is_root
 
